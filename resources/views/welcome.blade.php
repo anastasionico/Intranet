03346ccd5c9bfed5c8868c7d6674d2ae516a74b4
Nico <anastasionico@gmail.com>
2017-06-09 23:11:02 +0100

    creation layout welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,33 +64,98 @@
         <link rel="stylesheet" type="text/css" href="css/app.css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
+        <div class="container-full welcome-page">
+            <div class="halfwide  d-inline-block">
+                aaa
+            </div>
+            <div class="halfwide  d-inline-block" style="float:right;">
+              <div class="flex-center position-ref full-height">
+                    @if (Route::has('login'))
+                        <div class="top-right links">
+                            @if (Auth::check())
 
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        {{-- <a href="{{ url('/register') }}">Register</a> --}}
+                                <a href="{{ url('/home') }}">Home</a>
+                            @else
+                                <a href="{{ url('/login') }}">Login</a>
+                                {{-- <a href="{{ url('/register') }}">Register</a> --}}
+                            @endif
+                        </div>
                     @endif
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Intranet
-                    <h4>Imperial Commercials</h4>
-                </div>
+                    <div class="content">
+                        <div class="title m-b-md">
+                            Intranet
+                            <h4>Imperial Commercials</h4>
+                        </div>
+                        
+                        <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                                        {{ csrf_field() }}
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                                            <label for="username" class="col-md-4 control-label">Username</label>
+
+                                            <div class="col-md-6">
+                                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+
+                                                @if ($errors->has('username'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('username') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                                            <div class="col-md-6">
+                                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                                @if ($errors->has('password'))
+                                                    <span class="help-block">
+                                                        <strong>{{ $errors->first('password') }}</strong>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-4">
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="col-md-8 col-md-offset-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    Login
+                                                </button>
+
+                                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                    Forgot Your Password?
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        <div class="links">
+                            <a href="https://laravel.com/docs">Documentation</a>
+                            <a href="https://laracasts.com">Laracasts</a>
+                            <a href="https://laravel-news.com">News</a>
+                            <a href="https://forge.laravel.com">Forge</a>
+                            <a href="https://github.com/laravel/laravel">GitHub</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        
     </body>
 </html>
