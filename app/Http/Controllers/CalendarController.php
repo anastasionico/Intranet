51ecf24247a8cb16c3712d9d2a5ab5e598239f62
Nico <anastasionico@gmail.com>
2017-06-30 15:25:27 +0100

    fullcalendar added

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -12,14 +12,6 @@ class CalendarController extends Controller
     	$events = [];
 
 		$events[] = \Calendar::event(
-		    'Event One', //event title
-		    false, //full day event?
-		    '2017-03-11T0800', //start time (you can also use Carbon instead of DateTime)
-		    '2017-03-12T0800', //end time (you can also use Carbon instead of DateTime)
-			0 //optionally, you can specify an event ID
-		);
-
-		$events[] = \Calendar::event(
 		    "Valentine's Day", //event title
 		    true, //full day event?
 		    new \DateTime('2017-02-14'), //start time (you can also use Carbon instead of DateTime)
