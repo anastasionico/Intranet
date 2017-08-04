d569953ef2c7d2b51f831e94ddce2bf8e53c81c5
Nico <anastasionico@gmail.com>
2017-08-04 17:02:33 +0100

    migration of user using the role

---
+++ database/migrations/2017_08_04_160029_update-user-table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $t)
+        {
+            $t->dropColumn('job_title');
+            $t->integer('job_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $t){
+            $t->integer('job_title');
+            $t->dropColumn('job_id');
+        });
+    }
+}
